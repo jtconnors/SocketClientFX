54f096b311c06ec6f32b20c8d6a6f72edadd98d4
Jim Connors <github@jtconnors.com>
2016-10-22 22:57:26 -0400

    First Commit

---
+++ save/FXMLDocumentController.java
@@ -0,0 +1,301 @@
+package socketclientfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import socketfx.Constants;
+import socketfx.FxSocketClient;
+import socketfx.SocketListener;
+
+/**
+ * FXML Controller class
+ *
+ * @author jtconnor
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private ListView<String> rcvdMsgsListView;
+    @FXML
+    private ListView<String> sentMsgsListView;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextField sendTextField;
+    @FXML
+    private TextField selectedTextField;
+    @FXML
+    private Button connectButton;
+    @FXML
+    private Button disconnectButton;
+    @FXML
+    private TextField hostTextField;
+    @FXML
+    private TextField portTextField;
+    @FXML
+    private CheckBox autoConnectCheckBox;
+    @FXML
+    private TextField retryIntervalTextField;
+    @FXML
+    private Label connectedLabel;
+
+    private ObservableList<String> rcvdMsgsData;
+    private ObservableList<String> sentMsgsData;
+    private ListView lastSelectedListView;
+
+    private boolean connected;
+    private volatile boolean isAutoConnected;
+
+    private static final int DEFAULT_RETRY_INTERVAL = 2000; // in milliseconds
+
+    public enum ConnectionDisplayState {
+
+        DISCONNECTED, ATTEMPTING, CONNECTED, AUTOCONNECTED, AUTOATTEMPTING
+    }
+
+    private FxSocketClient socket;
+
+    /*
+     * Synchronized method set up to wait until there is no socket connection.
+     * When notifyDisconnected() is called, waiting will cease.
+     */
+    private synchronized void waitForDisconnect() {
+        while (connected) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    /*
+     * Synchronized method responsible for notifying waitForDisconnect()
+     * method that it's OK to stop waiting.
+     */
+    private synchronized void notifyDisconnected() {
+        connected = false;
+        notifyAll();
+    }
+
+    /*
+     * Synchronized method to set isConnected boolean
+     */
+    private synchronized void setIsConnected(boolean connected) {
+        this.connected = connected;
+    }
+
+    /*
+     * Synchronized method to check for value of connected boolean
+     */
+    private synchronized boolean isConnected() {
+        return (connected);
+    }
+
+    private void connect() {
+        socket = new FxSocketClient(new FxSocketListener(),
+                hostTextField.getText(),
+                Integer.valueOf(portTextField.getText()),
+                Constants.instance().DEBUG_NONE);
+        socket.connect();
+    }
+
+    private void autoConnect() {
+        new Thread() {
+            @Override
+            public void run() {
+                while (isAutoConnected) {
+                    if (!isConnected()) {
+                        socket = new FxSocketClient(new FxSocketListener(),
+                                hostTextField.getText(),
+                                Integer.valueOf(portTextField.getText()),
+                                Constants.instance().DEBUG_NONE);
+                        socket.connect();
+                    }
+                    waitForDisconnect();
+                    try {
+                        Thread.sleep(Integer.valueOf(
+                                retryIntervalTextField.getText()) * 1000);
+                    } catch (InterruptedException ex) {
+                    }
+                }
+            }
+        }.start();
+    }
+
+    private void displayState(ConnectionDisplayState state) {
+        switch (state) {
+            case DISCONNECTED:
+                connectButton.setDisable(false);
+                disconnectButton.setDisable(true);
+                sendButton.setDisable(true);
+                sendTextField.setDisable(true);
+                connectedLabel.setText("Not connected");
+                break;
+            case ATTEMPTING:
+            case AUTOATTEMPTING:
+                connectButton.setDisable(true);
+                disconnectButton.setDisable(true);
+                sendButton.setDisable(true);
+                sendTextField.setDisable(true);
+                connectedLabel.setText("Attempting connection");
+                break;
+            case CONNECTED:
+                connectButton.setDisable(true);
+                disconnectButton.setDisable(false);
+                sendButton.setDisable(false);
+                sendTextField.setDisable(false);
+                connectedLabel.setText("Connected");
+                break;
+            case AUTOCONNECTED:
+                connectButton.setDisable(true);
+                disconnectButton.setDisable(true);
+                sendButton.setDisable(false);
+                sendTextField.setDisable(false);
+                connectedLabel.setText("Connected");
+                break;
+        }
+    }
+
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setIsConnected(false);
+        isAutoConnected = false;
+        displayState(ConnectionDisplayState.DISCONNECTED);
+        
+        sentMsgsData = FXCollections.observableArrayList();
+        sentMsgsListView.setItems(sentMsgsData);
+        sentMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        sentMsgsListView.setOnMouseClicked((Event event) -> {
+            String selectedItem
+                    = sentMsgsListView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null && !selectedItem.equals("null")) {
+                selectedTextField.setText("Sent: " + selectedItem);
+                lastSelectedListView = sentMsgsListView;
+            }
+        });
+
+        rcvdMsgsData = FXCollections.observableArrayList();
+        rcvdMsgsListView.setItems(rcvdMsgsData);
+        rcvdMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        rcvdMsgsListView.setOnMouseClicked((Event event) -> {
+            String selectedItem
+                    = rcvdMsgsListView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null && !selectedItem.equals("null")) {
+                selectedTextField.setText("Received: " + selectedItem);
+                lastSelectedListView = rcvdMsgsListView;
+            }
+        });
+
+    }
+
+    class FxSocketListener implements SocketListener {
+
+        @Override
+        public void onMessage(String line) {
+            if (line != null && !line.equals("")) {
+                rcvdMsgsData.add(line);
+            }
+        }
+
+        @Override
+        public void onClosedStatus(boolean isClosed) {
+            if (isClosed) {
+                notifyDisconnected();
+                if (isAutoConnected) {
+                    displayState(ConnectionDisplayState.AUTOATTEMPTING);
+                } else {
+                    displayState(ConnectionDisplayState.DISCONNECTED);
+                }
+            } else {
+                setIsConnected(true);
+                if (isAutoConnected) {
+                    displayState(ConnectionDisplayState.AUTOCONNECTED);
+                } else {
+                    displayState(ConnectionDisplayState.CONNECTED);
+                }
+            }
+        }
+    }
+
+    @FXML
+    private void handleClearRcvdMsgsButton(ActionEvent event) {
+        rcvdMsgsData.clear();
+        if (lastSelectedListView == rcvdMsgsListView) {
+            selectedTextField.clear();
+        }
+    }
+
+    @FXML
+    private void handleClearSentMsgsButton(ActionEvent event) {
+        sentMsgsData.clear();
+        if (lastSelectedListView == sentMsgsListView) {
+            selectedTextField.clear();
+        }
+    }
+
+    @FXML
+    private void handleSendMessageButton(ActionEvent event) {
+        if (!sendTextField.getText().equals("")) {
+            socket.sendMessage(sendTextField.getText());
+            sentMsgsData.add(sendTextField.getText());
+        }
+    }
+
+    @FXML
+    private void handleConnectButton(ActionEvent event) {
+        displayState(ConnectionDisplayState.ATTEMPTING);
+        connect();
+    }
+
+    @FXML
+    private void handleDisconnectButton(ActionEvent event) {
+        socket.shutdown();
+    }
+
+    @FXML
+    private void handleAutoConnectCheckBox(ActionEvent event) {
+        if (autoConnectCheckBox.isSelected()) {
+            isAutoConnected = true;
+            if (isConnected()) {
+                displayState(ConnectionDisplayState.AUTOCONNECTED);
+            } else {
+                displayState(ConnectionDisplayState.AUTOATTEMPTING);
+                autoConnect();
+            }
+        } else {
+            isAutoConnected = false;
+            if (isConnected()) {
+                displayState(ConnectionDisplayState.CONNECTED);
+            } else {
+                displayState(ConnectionDisplayState.DISCONNECTED);
+            }
+        }
+    }
+
+    @FXML
+    private void handleRetryIntervalTextField(ActionEvent event) {
+        try {
+            Integer.parseInt(retryIntervalTextField.getText());
+        } catch (NumberFormatException ex) {
+            retryIntervalTextField.setText(
+                    Integer.toString(DEFAULT_RETRY_INTERVAL / 1000));
+        }
+    }
+}
---
+++ src/socketfx/FxSocketClient.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2016, Jim Connors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of this project nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package socketfx;
+
+import com.jtconnors.socket.Constants;
+import com.jtconnors.socket.DebugFlags;
+import com.jtconnors.socket.GenericSocket;
+import com.jtconnors.socket.SocketListener;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FxSocketClient extends GenericSocket
+        implements SocketListener {
+    
+    private final static Logger LOGGER =
+            Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    public String host;
+    private SocketListener fxListener;
+
+    /**
+     * Called whenever a message is read from the socket.  In
+     * JavaFX, this method must be run on the main thread and
+     * is accomplished by the Platform.runLater() call.  Failure to do so
+     * *will* result in strange errors and exceptions.
+     * @param line Line of text read from the socket.
+     */
+    @Override
+    public void onMessage(final String line) {
+        javafx.application.Platform.runLater(() -> {
+            fxListener.onMessage(line);
+        });
+    }
+
+    /**
+     * Called whenever the open/closed status of the Socket
+     * changes.  In JavaFX, this method must be run on the main thread and
+     * is accomplished by the Platform.runLater() call.  Failure to do so
+     * will* result in strange errors and exceptions.
+     * @param isClosed true if the socket is closed
+     */
+    @Override
+    public void onClosedStatus(final boolean isClosed) {
+        javafx.application.Platform.runLater(() -> {
+            fxListener.onClosedStatus(isClosed);
+        });
+    }
+
+    /**
+     * Initialize the SocketClient up to and including issuing the accept()
+     * method on its socketConnection.
+     * @throws java.net.SocketException
+     */
+    @Override
+    protected void initSocketConnection() throws SocketException {
+        try {
+            socketConnection = new Socket();
+            /*
+             * Allows the socket to be bound even though a previous
+             * connection is in a timeout state.
+             */
+            socketConnection.setReuseAddress(true);
+            /*
+             * Create a socket connection to the server
+             */
+            socketConnection.connect(new InetSocketAddress(host, getPort()));
+            if (debugFlagIsSet(DebugFlags.instance().DEBUG_STATUS)) {
+                LOGGER.log(Level.INFO, "Connected to {0}at port {1}",
+                        new Object[]{host, getPort()});
+            }
+        } catch (IOException e) {
+            if (debugFlagIsSet(DebugFlags.instance().DEBUG_EXCEPTIONS)) {
+                LOGGER.log(Level.SEVERE, ExceptionStackTraceAsString(e));
+            }
+            throw new SocketException();
+        }
+    }
+
+    /**
+     * For SocketClient class, no additional work is required.  Method
+     * is null.
+     */
+    @Override
+    protected void closeAdditionalSockets() {}
+    
+    public FxSocketClient(SocketListener fxListener,
+            String host, int port, int debugFlags) {
+        super(port, debugFlags);
+        this.host = host;
+        this.fxListener = fxListener;
+    }
+
+    public FxSocketClient(SocketListener fxListener) {
+        this(fxListener, Constants.instance().DEFAULT_HOST,
+                Constants.instance().DEFAULT_PORT,
+                DebugFlags.instance().DEBUG_NONE);
+    }
+
+    public FxSocketClient(SocketListener fxListener,
+            String host, int port) {
+        this(fxListener, host, port, DebugFlags.instance().DEBUG_NONE);
+    }
+}
