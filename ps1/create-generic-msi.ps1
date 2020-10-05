1f6ec6a4fb86153350f19aee1290331e9e4307ea
jtconnors <git@jtconnors.com>
2020-10-05 17:24:05 -0400

    Added ps1\create-generic-msi.ps1 script

---
+++ ps1/create-generic-msi.ps1
@@ -0,0 +1,49 @@
+
+####################
+#
+# All Scripts should have this preamble     
+#
+Set-variable -Name CMDLINE_ARGS -Value $args
+
+#
+# Move to the directory containing this script so we can source the env.ps1
+# properties that follow
+#
+$STARTDIR = pwd | Select-Object | %{$_.ProviderPath}
+cd $PSScriptRoot
+
+#
+# Common properties shared by scripts
+#
+. .\env.ps1
+if ($Global:JUST_EXIT -eq "true") {
+    cd $STARTDIR
+    Exit 1
+}
+#
+# End preamble
+#
+####################
+
+#
+# Run the Java command
+#
+Set-Variable -Name JPACKAGE_ARGS -Value @(
+    '-n',
+    """my-jdk-$EXPECTED_JDK_VERSION""",
+    '--type',
+    'msi',
+    '--vendor',
+    """jpackage-generated""",
+    '--runtime-image',
+    """$env:JAVA_HOME"""
+)
+if ($VERBOSE_OPTION -ne $null) {
+   $JPACKAGE_ARGS += '--verbose'
+}
+Exec-Cmd("$JPACKAGE_HOME\bin\jpackage.exe", $JPACKAGE_ARGS)
+
+#
+# Return to the original directory
+#
+cd $STARTDIR
